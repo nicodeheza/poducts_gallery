435b341f696bcb8e63bf4c896a5c54a8bc80c1e2
nicodeheza <nicolasdeheza@hotmail.com>
2022-05-27 18:37:51 -0300

    editProduct status 200, findByIdAndUpdate new true

--- server/controllers/editProduct.js
+++ server/controllers/editProduct.js
@@ -3,14 +3,18 @@ import Product from "../models/Product.js";
 export default async function editProduct(req, res) {
 	try {
 		const {_id, name, description, price, category, imageUrl} = req.body;
-		const updatedProduct = await Product.findByIdAndUpdate(_id, {
-			name,
-			description,
-			price,
-			category,
-			imageUrl
-		});
-		res.status(400).json(updatedProduct);
+		const updatedProduct = await Product.findByIdAndUpdate(
+			_id,
+			{
+				name,
+				description,
+				price,
+				category,
+				imageUrl
+			},
+			{new: true}
+		);
+		res.status(200).json(updatedProduct);
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({error});
