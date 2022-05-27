9a9635ff3dce04d87ddcc75f6dc31d49c4d0c445
nicodeheza <nicolasdeheza@hotmail.com>
2022-05-27 18:31:08 -0300

    add await in getProducts

--- server/controllers/getProducts.js
+++ server/controllers/getProducts.js
@@ -5,10 +5,10 @@ export default async function getProducts(req, res) {
 	//filtrado esta en la seccion de front-end por lo que
 	//lo hare ahi.
 	try {
-		const products = Product.find({});
+		const products = await Product.find({});
 		res.status(200).json(products);
-	} catch (error) {
+	} catch (err) {
 		console.log(err);
-		res.status(500).json({error});
+		res.status(500).json({error: err});
 	}
 }
