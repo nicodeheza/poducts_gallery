b024f666726fe3c65ba91ba3cc8233821bad69aa
nicodeheza <nicolasdeheza@hotmail.com>
2022-05-27 17:42:54 -0300

    routes

--- server/controllers/createProduct.js
+++ server/controllers/createProduct.js
@@ -1,6 +1,6 @@
 import Product from "../models/Product.js";
 
-export default async function createProducts(req, res) {
+export default async function createProduct(req, res) {
 	try {
 		const {name, description, price, imageUrl} = req.body;
 		const newProduct = new Product({
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,8 +1,14 @@
 import {Router} from "express";
+import createProduct from "../controllers/createProduct.js";
+import deleteProduct from "../controllers/deleteProduct.js";
+import editProduct from "../controllers/editProduct.js";
 import getProducts from "../controllers/getProducts.js";
 
 const router = Router();
 
 router.get("/", getProducts);
+router.post("/add", createProduct);
+router.put("/update", editProduct);
+router.delete("/delete", deleteProduct);
 
 export default router;
